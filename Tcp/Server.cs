a689ae99779916f2b949c740c0d6966282da008a
NirZamir1 <92547339+NirZamir1@users.noreply.github.com>
2023-12-28 14:54:53 +0200

    Update Server.cs

--- Tcp/Server.cs
+++ Tcp/Server.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 
-namespace P2P.Tcp
+namespace Server
 {
     public class Server
     {
